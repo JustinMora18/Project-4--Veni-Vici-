96e6d1c14584789d7c3ad250ebfa53a425ba05bf
JustinMora18 <justindaniel1810@gmail.com>
2025-07-08 12:52:47 -0400

    add Header component with title and description

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+function Header(){
+    return (
+        <header className="header">
+            <h1>Discover<br />Cocktails</h1>
+            <p className="description">
+                Shake up your senses with a random mix of drinks from around the world. Discover new favorites — or ban the ones you’d rather avoid.
+            </p>
+        </header>
+    )
+}
+
+export default Header
