f426e4e8419aa8741fa9335e736608573654bb06
JustinMora18 <justindaniel1810@gmail.com>
2025-07-08 14:09:25 -0400

    add functionality to fetch and display random drinks with SeenDrinks component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,35 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 import Header from './components/Header'
+import SeenDrinks from './components/SeenDrinks'
 
 function App() {
+  const [currentDrink, setCurrentDrink] = useState(null)
+  const [seenDrinks, setSeenDrinks] = useState([])
+
+  const fetchRandomDrink = async ( ) => {
+    try {
+      const response = await fetch('https://www.thecocktaildb.com/api/json/v1/1/random.php')
+      const data = await response.json()
+      const drink = data.drinks[0]
+      setCurrentDrink(drink)
+
+      setSeenDrinks(prev => [...prev, drink])
+    } catch (error) {
+      console.error('error fetching drink:', error)
+    }
+  }
+
+  useEffect(() => {
+    fetchRandomDrink()
+  }, [])
 
   return (
     <div className='app-container'>
       <Header />
+      <div className="content-container">
+        <SeenDrinks seenDrinks={seenDrinks} />
+      </div>
     </div>
   )
 }
---
+++ src/components/SeenDrinks.jsx
@@ -0,0 +1,17 @@
+function SeenDrinks({ seenDrinks }) {
+    return (
+        <div className="seen-drinks">
+            <h2 className="section-title">Drinks<br />you've seen</h2>
+            <div className="seen-list">
+                {seenDrinks.map((drink, index) => (
+                    <div className="seen-item">
+                        <img src={drink.strDrinkThumb} alt={drink.strDrink} />
+                        <p>{drink.strDrink}</p>
+                    </div>
+                ))}
+            </div>
+        </div>
+    )
+}
+
+export default SeenDrinks
